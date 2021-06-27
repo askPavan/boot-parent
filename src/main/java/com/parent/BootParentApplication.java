10b3223f91d16117bd5c9a8d2e2031861e7aea72
askpavan <askpavanit@gmail.com>
2021-06-27 23:52:26 +0530

    HIS-301:Related Changes

--- src/main/java/com/parent/BootParentApplication.java
+++ src/main/java/com/parent/BootParentApplication.java
@@ -7,10 +7,16 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class BootParentApplication {
 
+private Map<String, Object> cache = new HashMap<String, Object>();
+
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(BootParentApplication.class, args);
 		context.getClass();
 		// changes for 123
+	}
+	//HIS-301 changes
+	public void loadDataToCache(){
+		//logic to retrieve the data from database.
 	}
 
 }
