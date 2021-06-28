1e5d5a3f2ed1bd3829452a72ced16c055222fe47
askpavan <askpavanit@gmail.com>
2021-06-28 11:39:16 +0530

    commit

--- src/main/java/com/parent/beans/Car.java
+++ src/main/java/com/parent/beans/Car.java
@@ -7,11 +7,9 @@ public class Car {
 
 	public void start() {
 		System.out.println("Car started.... ");
-<<<<<<< HEAD
+
 	// changes for ALGR-122 and feature-301 changes. next commit
-=======
 	// changes for ALGR-122 and feature-301 changes
 	// changes for ALGR-122 and feature-300 changes
->>>>>>> 2a97b0637b5f2b8a45af246275823e1d065701ed
 	}
 }
