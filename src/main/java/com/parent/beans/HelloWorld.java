5c0d9b090e95fa7e3731263619e57ddadf19f688
askpavan <askpavanit@gmail.com>
2021-07-02 22:19:03 +0530

    Cleanedup the code changes.

--- src/main/java/com/parent/beans/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld{
-	public static void main(String... args){
-		System.out.println("This is first time coding in java using sublime text....");
-
-		List<String> list = new ArrayList<String>();
-		list.add("one");
-		System.out.println(list);
-	}
-}
--- src/main/java/com/parent/beans/Machine.java
+++ src/main/java/com/parent/beans/Machine.java
@@ -1,10 +1,11 @@
 package com.parent.beans;
 
+import org.springframework.stereotype.Component;
 
 @Component
 public class Machine{
 
 	public void start(){
-		System.out.println("machine started...")
+		System.out.println("machine started...");
 	}	
 }
